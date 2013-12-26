32af16b2c1fabd7edb6e7d21ed7a9ecf0a8cf074
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-12-27 07:40:44 +0900

    Add the standard elisp header

--- steady-theme.el
+++ steady-theme.el
@@ -1,6 +1,31 @@
-;;; -*- lexical-binding: t -*-
+;;; steady-theme.el --- a steady theme for Emacs
+
+;; Copyright (C) 2013  Yuta Taniguchi
+
+;; Author: Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
+;; Keywords: theme
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
 (deftheme steady
-  "Created 2013-12-08.")
+  "A steady theme for Emacs")
 
 (let ((white  "#ffffff")
       (gray   "gray70")
@@ -129,3 +154,8 @@
   (setq hl-paren-colors '("#ff0000" "#bf4040" "#9f6060" "#8f7070" "#808080")))
 
 (provide-theme 'steady)
+
+;; Local Variables:
+;; lexical-binding: t
+;; End:
+;;; steady-theme.el ends here
