90fe75dacd8147bd613582ad5b25a694dce8c19b
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2014-12-06 22:22:41 +0900

    Change the color of 'highlight'

--- steady-dark-theme.el
+++ steady-dark-theme.el
@@ -82,7 +82,7 @@
    `(powerline-buffer-id-insert   ((t :inherit (powerline-second-insert))))
    `(powerline-buffer-id-inactive ((t :inherit (powerline-second-inactive))))
    ;; Highlight
-   `(highlight           ((t :background "#133f55" :weight bold)))
+   `(highlight           ((t :background "gray23" :weight bold)))
    `(isearch             ((t :background ,bg-yellow :foreground "black" :box (:line-width -1 :color ,fg-yellow :style nil))))
    `(query-replace       ((t :inherit (isearch))))
    `(lazy-highlight      ((t :background nil :inherit (isearch))))
